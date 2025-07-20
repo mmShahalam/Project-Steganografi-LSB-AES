fdd02fc5c1305483e74bc8d12a22af24cff3fd31
Shah Alam <135126113+mmShahalam@users.noreply.github.com>
2025-07-20 16:04:20 +0700

    deployment setup

--- app.py
+++ app.py
@@ -266,4 +266,5 @@ def server_error(e):
     return render_template('500.html'), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=False, host='0.0.0.0', port=port)
